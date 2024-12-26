168132e237a6d45f30dafc73c12c1bd64a8aee5a
bartoszd96 <bartosz.drzygala@gmail.com>
2024-12-26 23:04:41 +0100

    Some additional changes
    Added transfer_wieznia function

--- pipeline/functions.sql
+++ pipeline/functions.sql
@@ -7,7 +7,7 @@ BEGIN
 	WHERE w.id_celi = NEW.id_celi 
 	AND w.id_placowki = NEW.id_placowki AND w.data_wyjscia IS NULL;
 	
-	SELECT p.pojemnosc_celi INTO pojemnosc FROM pomieszcznia p 
+	SELECT p.pojemnosc_celi INTO pojemnosc FROM pomieszczenia p 
 	WHERE p.typ_sali = 'cela' AND p.id_sali = NEW.id_celi 
 	AND p.id_placowki = NEW.id_placowki;
 	
@@ -20,5 +20,65 @@ BEGIN
     END IF;
 	
 	RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION DODAJ_CELE() RETURNS TRIGGER AS $$
+DECLARE 
+	Z1 INTEGER;
+	Z2 INTEGER;
+	Z3 INTEGER;
+BEGIN
+	
+	SELECT s.zmiana INTO Z1 FROM straznicy s 
+	WHERE s.id_straznika = NEW.id_straznika_1 and s.id_placowki = NEW.id_placowki;
+	
+	SELECT s.zmiana INTO Z2 FROM straznicy s 
+	WHERE s.id_straznika = NEW.id_straznika_2 and s.id_placowki = NEW.id_placowki;
+	
+	SELECT s.zmiana INTO Z3 FROM straznicy s 
+	WHERE s.id_straznika = NEW.id_straznika_3 and s.id_placowki = NEW.id_placowki;
+	
+	IF (Z1 = 1) AND (Z2 = 2) AND (Z3 = 3) THEN 
+		RETURN NEW;
+	ELSE 
+		RAISE EXCEPTION 'Błąd w danych, albo któryś strażnik nie istnieje, albo próbowano przypisać do go złej zmiany';
+	END IF;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION TRANSFER_WIEZNIA(id_w INTEGER, placowka_in INTEGER, cela_in INTEGER, placowka_out INTEGER, cela_out INTEGER) RETURNS VOID AS $$
+DECLARE
+	transferowany INTEGER;
+	ile_out INTEGER;
+	pojemnosc_out INTEGER;
+BEGIN
+	
+	SELECT count(*) INTO transferowany FROM wiezniowie w 
+	WHERE w.id_wieznia = id_w AND w.id_placowki = placowka_in AND w.id_celi = cela_in;
+	
+	SELECT count(*) INTO ile_out FROM wiezniowie w
+	WHERE w.id_celi = cela_out 
+	AND w.id_placowki = placowka_out AND w.data_wyjscia IS NULL;
+	
+	SELECT p.pojemnosc_celi INTO pojemnosc_out FROM pomieszczenia p 
+	WHERE p.typ_sali = 'cela' AND p.id_sali = cela_out 
+	AND p.id_placowki = placowka_out;
+	
+	IF (ile_out IS NULL) OR (pojemnosc_out IS NULL) THEN
+		RAISE EXCEPTION 'Niepoprawne dane, upewnij się czy więzień jest w danej placówce i celi, oraz że cela do której chcesz go przenieść istnieje i ma wolne miejsce';
+	END IF;
+	
+	IF (transferowany = 1) AND (pojemnosc_out > ile_out) THEN 
+		INSERT INTO TRANSFERY(id_placowki_in, id_placowki_out, id_celi_in, id_celi_out, id_wieznia) 
+		VALUES (placowka_in, placowka_out, cela_in, cela_out, id_w);
+		
+		UPDATE wiezniowie SET id_placowki = placowka_out, id_celi = cela_out 
+		WHERE id_wieznia = id_w;
+		
+	ELSE 
+		RAISE EXCEPTION 'Niepoprawne dane, upewnij się czy więzień jest w danej placówce i celi, oraz że cela do której chcesz go przenieść istnieje i ma wolne miejsce';
+	END IF;
+	
 END;
 $$ LANGUAGE 'plpgsql';
--- pipeline/load_data.sql
+++ pipeline/load_data.sql
@@ -2,11 +2,11 @@ INSERT INTO placowki(nazwa, adres, longitude, latitude) VALUES
 ('Zakład karny nr 1 we Wrocławiu', 'Kleczkowska 35, Wrocław 50-211', 51.127434, 17.0269411),
 ('Zakład karny w Wołowie', 'Więzienna 6, Wołow 56-100', 51.2581917, 16.5629008);
 
-INSERT INTO straznicy VALUES (zmiana, id_placowki, wyplata) VALUES 
-(1, 1, 5000), (2, 1, 5000), (3, 1, 5000);
+INSERT INTO straznicy(zmiana, id_placowki, wyplata) VALUES 
+(1, 1, 5000), (2, 1, 5000), (3, 1, 5000), (1, 1, 5000), (2, 1, 5000), (3, 1, 5000), (1, 2, 5000), (2, 2, 5000), (3, 2, 5000), (1, 2, 5000), (2, 2, 5000), (3, 2, 5000);
 
 INSERT INTO pomieszczenia(id_placowki, typ_sali, pojemnosc_magazynu, pojemnosc_celi, id_straznika_1, id_straznika_2, id_straznika_3) VALUES 
-(1, 'cela', NULL, 6, 1, 2, 3);
+(1, 'cela', NULL, 6, 1, 2, 3), (1, 'cela', NULL, 6, 4, 5, 6), (2, 'cela', NULL, 4, 7, 8, 9), (2, 'cela', NULL, 4, 10, 11, 12);
 
 INSERT INTO PRZESTEPSTWA (wykroczenie, stopien_zagrozenia)
 VALUES 
@@ -27,10 +27,19 @@ VALUES
     (5, 'Michał', 'Wójcik', 'Fałszerz', 'Fałszerstwo', '2024-05-25', 3, 'Fałszerze Profesjonalni', 1, 1, 1, NULL, 'zdjecie5.jpg'),
     (6, 'Tomasz', 'Kamiński', 'Przemytnik', 'Przemyt', '2024-06-30', 6, 'Gang Przemytników', 1, 1, 1, NULL, 'zdjecie6.jpg');
 	
--- TEST triggera	
+-- TEST triggera dodaj wieznia
 	
 INSERT INTO WIEZNIOWIE (id_wieznia, imie, nazwisko, pseudonim, wykroczenie, data_przybycia, wyrok, gang, id_celi, id_stolowki, id_placowki, data_wyjscia, adres_zdjecia)
 VALUES
 	 (7, 'Krzysztof', 'Lewandowski', 'Zakłócacz', 'Zakłócanie porządku', '2024-07-15', 1, 'Gang Hałasowników', 1, 1, 1, NULL, 'zdjecie7.jpg');
 	
-	
+-- TEST triggera dodaj cele
+
+INSERT INTO pomieszczenia(id_placowki, typ_sali, pojemnosc_magazynu, pojemnosc_celi, id_straznika_1, id_straznika_2, id_straznika_3) VALUES 
+(1, 'cela', NULL, 6, 7, 8, 9);
+
+-- TEST transfer_wieznia
+
+select transfer_wieznia(1, 1, 1, 2, 1); -- powinno nie przejsc
+
+select transfer_wieznia(1, 1, 1, 2, 2); -- powinno przejsc
