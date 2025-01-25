79604cc8fb58ad986a670136012dd1eceb12ec29
koszmarna <koszmartamk@gmail.com>
2025-01-25 16:43:40 +0100

    Update views.sql

--- pipeline/views.sql
+++ pipeline/views.sql
@@ -1,12 +1,52 @@
-CREATE VIEW AS wydatki
-SELECT id_transakcji, kwota, data_transakcji, id_odbiorcy, 'kontrahent' AS status, id_kontrahenta, nazwa
+CREATE VIEW wydatki AS
+SELECT 
+    id_transakcji, 
+    kwota, 
+    data_transakcji, 
+    f.id_odbiorcy, 
+    'kontrahent' AS rodzaj_odbiorcy, 
+    id_kontrahenta AS id_podmiotu, 
+    nazwa AS nazwa_podmiotu
 FROM KONTRAHENCI k
-JOIN FINANSE f ON f.id_odbiorcy=k.id_odbiorcy;
+JOIN FINANSE f ON f.id_odbiorcy = k.id_odbiorcy
+
 UNION
-SELECT id_transakcji, kwota, data_transakcji, id_odbiorcy, 'pracownik administracyjny' AS status, id_pracownika, nazwisko, id_odbiorcy, nazwisko
+
+SELECT 
+    id_transakcji, 
+    kwota, 
+    data_transakcji, 
+    f.id_odbiorcy, 
+    'pracownik administracyjny' AS rodzaj_odbiorcy, 
+    id_pracownika AS id_podmiotu, 
+    nazwisko AS nazwa_podmiotu
 FROM pozostali_pracownicy p
-JOIN FINANSE f ON f.id_odbiorcy=p.id_odbiorcy;
+JOIN FINANSE f ON f.id_odbiorcy = p.id_odbiorcy
+
 UNION
-SELECT id_transakcji, kwota, data_transakcji, id_odbiorcy, 'straznik' AS status, id_straznika, nazwisko
+
+SELECT 
+    id_transakcji, 
+    kwota, 
+    data_transakcji, 
+    f.id_odbiorcy, 
+    'straznik' AS rodzaj_odbiorcy, 
+    id_straznika AS id_podmiotu, 
+    nazwisko AS nazwa_podmiotu
 FROM STRAZNICY s
-JOIN FINANSE f ON f.id_odbiorcy=s.id_odbiorcy;
+JOIN FINANSE f ON f.id_odbiorcy = s.id_odbiorcy;
+
+
+
+
+CREATE VIEW stan_cele AS
+SELECT 
+    c.id_celi, 
+    COUNT(w.id_wieznia) OVER (PARTITION BY c.id_celi) * 1.0 / c.pojemnosc_celi AS zapelnienie_celi,
+	z.id_straznika AS pilnujacy_straznik,
+    w.id_wieznia, 
+    w.imie, 
+    w.nazwisko
+FROM cele c
+LEFT JOIN wiezniowie w ON c.id_celi = w.id_celi
+JOIN zmiany z ON z.id_sektor=c.id_sektor;
